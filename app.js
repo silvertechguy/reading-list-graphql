ffbf044844aab4ac256438aadd3f5c1e14c2c5fb
Ali Fadda <silvertechguy@gmail.com>
2020-10-17 15:45:12 +0200

    setting up graphql

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
 
 const app = express();
+
+app.use("/graphql", graphqlHTTP({}));
 
 const PORT = process.env.PORT || process.env.port || 400;
 app.listen(PORT, () =>
