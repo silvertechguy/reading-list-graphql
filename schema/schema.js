e1fcf05dc85df23a345fb7c244e78dbadbb35b3e
Ali Fadda <silvertechguy@gmail.com>
2020-10-17 18:12:12 +0200

    author type

--- schema/schema.js
+++ schema/schema.js
@@ -3,6 +3,7 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
+  GraphQLInt,
 } = require("graphql");
 const _ = require("lodash");
 
@@ -12,12 +13,27 @@ const books = [
   { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
 ];
 
+const authors = [
+  { name: "Patrick Rothfuss", age: 44, id: "1" },
+  { name: "Brandon Sanderson", age: 42, id: "2" },
+  { name: "Terry Pratchett", age: 66, id: "3" },
+];
+
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -30,6 +46,14 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         // code to get data from db/ other source
         return _.find(books, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // code to get data from db/ other source
+        return _.find(authors, { id: args.id });
       },
     },
   },
