93ca3d1a8d6b32db41de5d2b4f80ea644792fd7d
Ali Fadda <silvertechguy@gmail.com>
2020-10-17 17:50:32 +0200

    testing queries in graphiql

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const graphqlHTTP = require("express-graphql");
+const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({ schema }));
+app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
-const PORT = process.env.PORT || process.env.port || 400;
+const PORT = process.env.PORT || process.env.port || 4000;
 app.listen(PORT, () =>
   console.log(`Now listening for requests on port ${PORT}`)
 );
